defe5239dbd39255d6cf535088ef6888be65e5cd
William Ryuputra <williamryuputra@yahoo.co.id>
2023-12-18 13:50:29 -0500

    Adds title to projects

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -26,11 +26,10 @@ const Projects = () => {
     const projects=[
         {
             projectId:1,
-            projectName:"3D Circuitry Internship",
+            projectName:"3D Circuitry Website",
             backgroundColor:"bg-[#FFDF8C]",
             imgPath:"/3d_laptop.png",
             gridSize:"md:col-span-2",
-            // width:"30%",
             width:300,
             reference:ref1,
             inView:inView
@@ -41,29 +40,26 @@ const Projects = () => {
             backgroundColor:"bg-[#BBA9D1]",
             imgPath:"/to-do.png",
             gridSize:"md:col-span-1",
-            // width:"30%",
             width:150,
             reference:ref2,
             inView:inView2
         },
         {
             projectId:3,
-            projectName:"Weather App",
+            projectName:"Weather Forecast",
             backgroundColor:"bg-[#7488B9]",
             imgPath:"/weather.png",
             gridSize:"md:col-span-1",
-            // width:"30%",
             width:300,
             reference:ref3,
             inView:inView3
         },
         {
             projectId:4,
-            projectName:"Refrigerant Inventory System",
+            projectName:"Refrigerant System",
             backgroundColor:"bg-[#7BA880]",
             imgPath:"/capstone.png",
             gridSize:"md:col-span-2",
-            // width:"30%",
             width:300,
             reference:ref4,
             inView:inView4
@@ -77,8 +73,9 @@ const Projects = () => {
                 {projects.map((project)=>{
                             return(
                                 <Link ref={project.reference} key={project.projectId} className={`${project.gridSize} rounded-3xl row-span-2 opacity-0 transition-all duration-[1500ms] relative top-8 ${project.inView ? "show" : ""} flex justify-center items-center `} href={"/about"}>
-                                    <li className={`flex items-center justify-center rounded-2xl  ${project.backgroundColor} w-full h-full scale-100 transition-all ease-linear lg:hover:scale-105 `}>
-                                        <Image  src={project.imgPath} width={project.width} height={project.width} alt={`Project ${project.projectName}`}/>
+                                    <li className={` flex items-center justify-center rounded-2xl  ${project.backgroundColor} w-full h-full transition-all ease-linear lg:hover:scale-105 `}>
+                                        <Image className="" src={project.imgPath} width={project.width} height={project.width} alt={`Project ${project.projectName}`}/>
+                                        <p className="absolute bottom-0 py-6 md:p-10  md:bottom-0 md:right-0 white text-white text-xl text-center md:text-start">{`${project.projectName} ↗`}</p>
                                     </li>
                                 </Link>
                             )
